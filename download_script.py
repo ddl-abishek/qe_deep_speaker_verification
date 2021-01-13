fbe51a966626e4177a1bd7f1119358ca8494b539
Abishek Subramanian <abishek.subramanian@dominodatalab.com>
2021-01-12 18:08:18 -0800

    download_script
    Pushed from Domino: https://staging.domino.tech/u/abishek_subramanian/qe_deep_speaker_verification/runs/5ffe4ed1d86ad8109862c984

---
+++ download_script.py
@@ -0,0 +1,3 @@
+import os
+print('running wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partaa')
+os.system('wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox1_dev_wav_partaa')
