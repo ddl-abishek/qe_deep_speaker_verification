b0badec84983d9ce3167dcefa3993883a01ed6eb
Abishek Subramanian <abishek.subramanian@dominodatalab.com>
2021-01-27 18:21:34 -0800

    Pushed from Domino: https://staging.domino.tech/u/abishek_subramanian/qe_deep_speaker_verification_v2/runs/60121a5f793d3675116c1089

--- replicate_ckpt.py
+++ replicate_ckpt.py
@@ -1,5 +1,5 @@
 import os
 
-for i in range(10):
-    cmd = f'cp ./checkpoints/checkpoint_vivek ./checkpoints/checkpoint_vivek{i}'
+for i in range(50):
+    cmd = f'cp ./checkpoints/checkpoint_vivek ./checkpoints/checkpoint_vivek{4899+i}'
     os.system(cmd)
