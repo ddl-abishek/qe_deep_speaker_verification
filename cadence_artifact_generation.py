5e2d5e5e05df9310e3b8a571e612eecbb769485f
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2021-05-12 07:27:24 -0700

    Pushed from Domino: /u/integration-test/qe_deep_speaker_verification/runs/609bdea0f571162aaea85b4f

---
+++ cadence_artifact_generation.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import time
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("num_artifacts",help="number of artifacts/textfiles to generate",type=int)
+args = parser.parse_args()
+
+for _ in range(args.num_artifacts):
+    os.system(f"sudo touch /mnt/artifacts/results/{str(datetime.now())}.txt")
+    time.sleep(5)
