3d67748388cfa3e752b2dc419695aba9e51b26e7
ddl-abishek <71837717+ddl-abishek@users.noreply.github.com>
2021-07-30 18:46:08 +0530

    Update train_speech_embedder_norm.py

--- train_speech_embedder_norm.py
+++ train_speech_embedder_norm.py
@@ -13,7 +13,7 @@ from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 
 def train(model_path):
-	writer = SummaryWriter(log_dir='/mnt/data/${DOMINO_PROJECT_NAME}/tensorboard_logs')
+	writer = SummaryWriter(log_dir=f'/mnt/data/{os.environ['DOMINO_PROJECT_NAME']}/tensorboard_logs')
 	device = torch.device(hp.device)
 
 	if hp.data.data_preprocessed:
